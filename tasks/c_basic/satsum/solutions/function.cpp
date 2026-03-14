15039b548a84cbe82335fb455d1e7db920dbe2b7
Игорь Минеев <mineev@optain.ru>
2026-03-14 13:58:48 +0000

    Clenup

--- tasks/c_basic/satsum/solutions/function.cpp
+++
@@ -1,7 +0,0 @@
-
-unsigned satsum(unsigned v1, unsigned v2)
-{
-    unsigned res = v1 + v2;
-    if (res < v1 || res < v2) res = ~0;
-    return res;
-}
--- tasks/c_basic/satsum/solutions/function2.cpp
+++
@@ -1,9 +0,0 @@
-
-unsigned satsum(unsigned v1, unsigned v2)
-{
-    unsigned res;
-    if (__builtin_uadd_overflow(v1, v2, &res)) {
-        res = ~0;
-    }
-    return res;
-}
