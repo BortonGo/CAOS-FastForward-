1c04bd4d6e058bdd080faa00b4ad04b37b00a13c
AlexeyGostyunin <bortone52@yandex.ru>
2026-03-16 09:29:33 +0300

    Add solution for average

--- tasks/c_basic/average/solution.c
+++ tasks/c_basic/average/solution.c
@@ -1,2 +1,16 @@
+#include "stdio.h"
+
 int main() {
+    unsigned a, b, c;
+
+    if (scanf("%u %u", &a, &b) != 2) {
+        return 1;
+    }
+
+    c = (a & b) + ((a ^ b) >> 1);
+
+    if (printf("%u\n", c) < 0) {
+        return 1;
+    }
+    return 0;
 }
