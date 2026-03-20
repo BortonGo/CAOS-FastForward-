36465ca7ab12fa8ece9ca8c482ca167a75241510
AlexeyGostyunin <bortone52@yandex.ru>
2026-03-20 14:36:36 +0300

    Add solution for mystrcmp

--- tasks/c_basic/mystrcmp/solution.c
+++ tasks/c_basic/mystrcmp/solution.c
@@ -1,2 +1,8 @@
 int mystrcmp(const char *str1, const char *str2) {
+    while (*str1 && *str2 && *str1 == *str2) {
+        ++str1;
+        ++str2;
+    }
+
+    return (unsigned char)*str1 - (unsigned char)*str2;
 }
