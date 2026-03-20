2d725201f2e2c20e3dd473dec8882989d21a09e9
AlexeyGostyunin <bortone52@yandex.ru>
2026-03-20 14:43:35 +0300

    Add solution for normalize-path

--- tasks/c_basic/normalize-path/solution.c
+++ tasks/c_basic/normalize-path/solution.c
@@ -1,2 +1,21 @@
-void normalize_path(char *path) {
+void normalize_path(char *buf) {
+    char *read = buf;
+    char *write = buf;
+
+    int prev_was_slash = 0;
+
+    while (*read) {
+        if (*read == '/') {
+            if (!prev_was_slash) {
+                *write++ = '/';
+                prev_was_slash = 1;
+            }
+        } else {
+            *write++ = *read;
+            prev_was_slash = 0;
+        }
+        ++read;
+    }
+
+    *write = '\0';
 }
