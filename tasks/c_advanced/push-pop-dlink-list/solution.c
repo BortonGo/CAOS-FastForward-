cae5198d1e3b83c195160ed89a9f84684b45cd81
AlexeyGostyunin <bortone52@yandex.ru>
2026-04-05 15:58:42 +0300

    Add solution push-pop

--- tasks/c_advanced/push-pop-dlink-list/solution.c
+++ tasks/c_advanced/push-pop-dlink-list/solution.c
@@ -3,9 +3,38 @@
 #include "dlist.h"
 
 Node *push(Node *place, int value) {
-    // TODO your code here
+    Node *new_node = (Node *)calloc(1, sizeof(*new_node));
+    if (new_node == NULL) {
+        return NULL;
+    }
+
+    new_node->value = value;
+    new_node->next = place;
+
+    if (place != NULL) {
+        new_node->prev = place->prev;
+
+        if (place->prev != NULL) {
+            place->prev->next = new_node;
+        }
+
+        place->prev = new_node;
+    }
+
+    return new_node;
 }
 
 int pop(Node *node) {
-    // TODO your code here
+    int value = node->value;
+
+    if (node->prev != NULL) {
+        node->prev->next = node->next;
+    }
+
+    if (node->next != NULL) {
+        node->next->prev = node->prev;
+    }
+
+    free(node);
+    return value;
 }
